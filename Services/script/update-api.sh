da92e745e5539c0570632d55f6d4ee0de7f2da44
Julien Boubechtoula <julien.boubechtoula@gmail.com>
2019-12-24 23:50:57 +0100

    update swagger

--- Services/script/update-api.sh
+++ Services/script/update-api.sh
@@ -5,12 +5,14 @@ export VERSION="1.1.0-SNAPSHOT"
 export APIS=( core resourcemanager appmanager binaryrepositorymanager catalogmanager pipelinemanager )
 export PROJECT_BASE=$SCRIPT_DIR/../../../
 
+docker run -p 8080:8080 -e ARTIFACT_REPOSITORY_URL="none" -e ARTIFACT_PIP_REPOSITORY_NAME="none" kathra/codegen-swagger:dev
+
 function updateSwaggerSpecsToSourceRepository() {
     for apiName in "${APIS[@]}"
     do
         fileApi=$SCRIPT_DIR/../$apiName/swagger.yaml
-        echo "Update $fileApi to $PROJECT_BASE/kathra-$apiName-api/swagger.yaml"
-        cp $fileApi $PROJECT_BASE/kathra-$apiName-api/swagger.yaml
+        echo "Update $fileApi to $PROJECT_BASE/kathra-$apiName-api/swagger.yml"
+        cp $fileApi $PROJECT_BASE/kathra-$apiName-api/swagger.yml
         [ $? -ne 0 ] && echo "Error" && exit 1
         [ -d $PROJECT_BASE/kathra-$apiName-model ] && codegen $fileApi $PROJECT_BASE/kathra-$apiName-model $VERSION $apiName "model"
         [ -d $PROJECT_BASE/kathra-$apiName-client ] && codegen $fileApi $PROJECT_BASE/kathra-$apiName-client $VERSION $apiName "client"
