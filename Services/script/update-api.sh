a049346b6d4e1451a988ff0a08deee6f58db6a3c
Julien Boubechtoula <julien.boubechtoula@gmail.com>
2019-12-26 12:07:42 +0100

    CatalogManager - Upgrade from specs

--- Services/script/update-api.sh
+++ Services/script/update-api.sh
@@ -5,7 +5,36 @@ export VERSION="1.1.0-SNAPSHOT"
 export APIS=( core resourcemanager appmanager binaryrepositorymanager catalogmanager pipelinemanager )
 export PROJECT_BASE=$SCRIPT_DIR/../../../
 
-docker run -p 8080:8080 -e ARTIFACT_REPOSITORY_URL="none" -e ARTIFACT_PIP_REPOSITORY_NAME="none" kathra/codegen-swagger:dev
+#docker run -p 8080:8080 -e ARTIFACT_REPOSITORY_URL="none" -e ARTIFACT_PIP_REPOSITORY_NAME="none" kathra/codegen-swagger:dev
+
+function main() {
+    [ $# -eq 0 ] && updateSwaggerSpecsToSourceRepository && return 0
+
+    component=$(findInArgs "--component" $*)
+    lib=$(findInArgs "--lib" $*)
+    echo $*
+    [ ! "$component" == "" ] && [ ! "$lib" == "" ] && updateComponent "$component" "$lib"
+}
+
+function updateComponent() {
+    local component=$1
+    local lib=$2
+    local fileApi=$SCRIPT_DIR/../$component/swagger.yaml
+    codegen "$fileApi" "$PROJECT_BASE/kathra-$component-$lib" "$VERSION" "$component" "$lib"
+}
+
+function findInArgs() {
+    local keyToFind=$1
+    shift 
+    POSITIONAL=()
+    while [[ $# -gt 0 ]]
+    do
+        [ "$(echo "$1" | cut -d'=' -f1)" == "${keyToFind}" ] && echo $(echo "$1" | cut -d'=' -f2) && return 0
+        shift
+    done
+    return 1
+}
+
 
 function updateSwaggerSpecsToSourceRepository() {
     for apiName in "${APIS[@]}"
@@ -15,8 +44,8 @@ function updateSwaggerSpecsToSourceRepository() {
         cp $fileApi $PROJECT_BASE/kathra-$apiName-api/swagger.yml
         [ $? -ne 0 ] && echo "Error" && exit 1
         [ -d $PROJECT_BASE/kathra-$apiName-model ] && codegen $fileApi $PROJECT_BASE/kathra-$apiName-model $VERSION $apiName "model"
-        [ -d $PROJECT_BASE/kathra-$apiName-client ] && codegen $fileApi $PROJECT_BASE/kathra-$apiName-client $VERSION $apiName "client"
-        [ -d $PROJECT_BASE/kathra-$apiName-interface ] && codegen $fileApi $PROJECT_BASE/kathra-$apiName-interface $VERSION $apiName "interface"
+        [ ! "$apiName" == "core" ] && [ -d $PROJECT_BASE/kathra-$apiName-client ] && codegen $fileApi $PROJECT_BASE/kathra-$apiName-client $VERSION $apiName "client"
+        [ ! "$apiName" == "core" ] && [ -d $PROJECT_BASE/kathra-$apiName-interface ] && codegen $fileApi $PROJECT_BASE/kathra-$apiName-interface $VERSION $apiName "interface"
     done
 }
 export -f updateSwaggerSpecsToSourceRepository
@@ -37,11 +66,15 @@ function codegen() {
     -H 'content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' \
     -F "apiFile=@$apiFile" > generated-code.zip
     [ $? -ne 0 ] && echo "Error" && exit 1
-    
+    # clean
+    rm -Rf src
+    # Backup pom
+    mv pom.xml pom.xml.backup
     unzip -o generated-code.zip
+    mv pom.xml.backup pom.xml
     [ $? -ne 0 ] && echo "Error" && exit 1
     rm generated-code.zip
 }
 export -f codegen
 
-updateSwaggerSpecsToSourceRepository
+main "$1" "$2" "$3" "$4" "$5"
